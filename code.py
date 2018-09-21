c3f241d4ebf2d23f0864ba42f072b61e6d9a6f9e
Marco Tamassia <tamassia.marco@gmail.com>
2018-09-21 12:13:15 +1000

    ENHANCE: update post and notebook

---
+++ code.py
@@ -0,0 +1,10 @@
+import re
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+from copy import deepcopy
+from imageio import imread
+from matplotlib.pyplot import imshow
+from plotly.offline import init_notebook_mode, iplot, plot
+init_notebook_mode(connected=True)
